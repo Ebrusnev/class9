6c952de9bda03d8528e0e52c8363580ddc2b87bd
Ebrusnev <evgenii.brusnev@gmail.com>
2017-04-03 21:14:46 -0500

    files apdated

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var express = require('express');
+var parser = require('body-parser')
+var _ = require('underscore');
+var ejs = require('ejs');
+var fs = require('fs');
+
 var app = express();
+app.use(parser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-	
-})
+	fs.readFile('./data.json', 'utf8', function(err, data){
+		if(err){
+			console.log(err);
+		}
+		res.render('landing', {articles: JSON.parse(data)});
+	});
+});
 
 app.listen(3030, function() {
 	console.log('Server is listenin on port 3030!');
-})
+});
